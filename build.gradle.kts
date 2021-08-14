c5b35bf3e27f9e4c9bb7aeacb183ebcb72077118
Alexey Lapin <alexey-lapin@protonmail.com>
2021-08-14 19:20:09 +0300

    add versions plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     application
+    id("com.github.ben-manes.versions")
     id("com.github.johnrengelman.shadow")
 }
 
@@ -24,4 +25,22 @@ application {
 
 tasks.test {
     useJUnitPlatform()
+}
+
+tasks {
+    dependencyUpdates {
+        checkConstraints = true
+        resolutionStrategy {
+            componentSelection {
+                all {
+                    val rejected = listOf("alpha", "beta", "rc", "cr", "m", "preview", "b", "ea")
+                        .map { qualifier -> Regex("(?i).*[.-]$qualifier[.\\d-+]*") }
+                        .any { it.matches(candidate.version) }
+                    if (rejected) {
+                        reject("Release candidate")
+                    }
+                }
+            }
+        }
+    }
 }
